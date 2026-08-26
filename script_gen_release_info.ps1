ab9cca07c7d7a9858e955aa66afa23f610e99472
counter185 <33550839+counter185@users.noreply.github.com>
2026-08-26 09:46:20 +0200

    autobuilds

--- script_gen_release_info.ps1
+++ script_gen_release_info.ps1
@@ -1,10 +1,15 @@
 $PathReleaseInfo = "ReencGUI/ReleaseInfo.cs"
+$PathReleaseInfoLinux = "reika.Linux/ReleaseInfo.cs"
 
 $textNow = [System.IO.File]::ReadAllText($PathReleaseInfo)
-
 $textNow = $textNow -replace '"[^"]*"\/\*HINT_VERSION_INFO\*\/', ('"'+ $env:VERSION_NAME +'"')
 $textNow = $textNow -replace '"[^"]*"\/\*HINT_GIT_REF\*\/', ('"'+ $env:VERSION_REF +'"')
-
 Write-Output "Writing: $textNow"
-
 [System.IO.File]::WriteAllText($PathReleaseInfo, $textNow)
+
+
+$textNow = [System.IO.File]::ReadAllText($PathReleaseInfoLinux)
+$textNow = $textNow -replace '"[^"]*"\/\*HINT_VERSION_INFO\*\/', ('"'+ $env:VERSION_NAME +'"')
+$textNow = $textNow -replace '"[^"]*"\/\*HINT_GIT_REF\*\/', ('"'+ $env:VERSION_REF +'"')
+Write-Output "Writing: $textNow"
+[System.IO.File]::WriteAllText($PathReleaseInfoLinux, $textNow)
